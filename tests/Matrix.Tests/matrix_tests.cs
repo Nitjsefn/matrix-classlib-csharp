6f3b188a08ee2a3cbbd24de46ca90b647311b1e0
Nitjsefn <>
2023-06-22 00:18:21 +0200

    created test for checking Matrix.ToString() method

--- tests/Matrix.Tests/matrix_tests.cs
+++ tests/Matrix.Tests/matrix_tests.cs
@@ -5,5 +5,14 @@ namespace Matrix.Tests
 {
 	public class matrix_tests
 	{
+		[Fact]
+		public void converts_to_string()
+		{
+			const string outputStr = "0 0 0\n0 0 0\n0 0 0\n";
+			int rows = 3;
+			int cols = 3;
+			var m = new Matrix(rows, cols);
+			Assert.Equal(outputStr, m.ToString());
+		}
 	}
 }

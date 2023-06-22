8d28b9ba159b0f71ff901060fd83a123d47dfa96
Nitjsefn <>
2023-06-22 21:32:17 +0200

    override toString() method in Matrix classlib and made tests pass

--- src/Matrix/Matrix.cs
+++ src/Matrix/Matrix.cs
@@ -2,5 +2,28 @@ namespace Matrix
 {
 	public class Matrix
 	{
+		public double[,] Elements { get; }
+		public int Rows { get; }
+		public int Cols { get; }
+
+		public Matrix(int rows, int cols)
+		{
+			Elements = new double[rows, cols];
+			Rows = rows;
+			Cols = cols;
+		}
+
+		public override string ToString()
+		{
+			string toRet = "";
+			for(int r = 0; r < Rows; r++)
+			{
+				for(int c = 0; c < Cols; c++)
+					toRet += Elements[r, c] + " ";
+				toRet = toRet.TrimEnd(' ');
+				toRet += '\n';
+			}
+			return toRet;
+		}
 	}
 }
